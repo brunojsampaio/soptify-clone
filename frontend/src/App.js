ce773ca96bbfa63db75167b83786da45ed03cac9
Bruno Sampaio <brunojsampaio@gmail.com>
2019-09-10 23:32:06 -0300

    Estilizando Sidebar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,9 +2,12 @@ import React from 'react';
 
 import GlobalStyle from './styles/global';
 
+import Sidebar from './components/Sidebar';
+
 const App = () => (
   <>
     <GlobalStyle />
+    <Sidebar />
   </>
 );
 
